977bdbe182a3a9835c4c5c3fd312ad45688947e6
Gabriel Augustin <gabriel.augustin@edu.sc.senai.br>
2024-03-25 11:15:51 -0300

    feat: Melhorias no cadastro de agendamentos e materiais

--- src/main/java/br/com/fullstack/education/m1s10exercicios/repositories/MaterialRepository.java
+++ src/main/java/br/com/fullstack/education/m1s10exercicios/repositories/MaterialRepository.java
@@ -1,7 +1,13 @@
 package br.com.fullstack.education.m1s10exercicios.repositories;
 
+import br.com.fullstack.education.m1s10exercicios.entities.AgendaEntity;
 import br.com.fullstack.education.m1s10exercicios.entities.MaterialEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface MaterialRepository extends JpaRepository<MaterialEntity, Long> {
+
+    List<MaterialEntity> findByAgenda(AgendaEntity agenda);
+
 }
--- src/main/java/br/com/fullstack/education/m1s10exercicios/services/MaterialServiceImpl.java
+++ src/main/java/br/com/fullstack/education/m1s10exercicios/services/MaterialServiceImpl.java
@@ -1,5 +1,6 @@
 package br.com.fullstack.education.m1s10exercicios.services;
 
+import br.com.fullstack.education.m1s10exercicios.entities.AgendaEntity;
 import br.com.fullstack.education.m1s10exercicios.entities.MaterialEntity;
 import br.com.fullstack.education.m1s10exercicios.exceptions.NotFoundException;
 import br.com.fullstack.education.m1s10exercicios.repositories.MaterialRepository;
@@ -10,9 +11,11 @@ import java.util.List;
 @Service
 public class MaterialServiceImpl implements MaterialService {
 
+    private final AgendaService agendaService;
     private final MaterialRepository repository;
 
-    public MaterialServiceImpl(MaterialRepository repository) {
+    public MaterialServiceImpl(AgendaService agendaService, MaterialRepository repository) {
+        this.agendaService = agendaService;
         this.repository = repository;
     }
 
@@ -30,8 +33,18 @@ public class MaterialServiceImpl implements MaterialService {
     }
 
     @Override
+    public List<MaterialEntity> buscarPorAgenda(Long agendaId) {
+        AgendaEntity agenda = agendaService.buscarPorId(agendaId);
+        return repository.findByAgenda(agenda);
+    }
+
+    @Override
     public MaterialEntity criar(MaterialEntity entity) {
         entity.setId(null);
+
+        AgendaEntity agenda = agendaService.buscarPorId(entity.getAgenda().getId());
+        entity.setAgenda(agenda);
+
         return repository.save(entity);
     }
 
@@ -39,6 +52,10 @@ public class MaterialServiceImpl implements MaterialService {
     public MaterialEntity alterar(Long id, MaterialEntity entity) {
         buscarPorId(id);
         entity.setId(id);
+
+        AgendaEntity agenda = agendaService.buscarPorId(entity.getAgenda().getId());
+        entity.setAgenda(agenda);
+
         return repository.save(entity);
     }
 
